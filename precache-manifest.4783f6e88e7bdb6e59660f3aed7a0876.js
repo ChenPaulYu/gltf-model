58f92f47224a863d035e8df3010c5b78eac81530
ChenPaulYu <beasoftwarengineering@gmail.com>
2019-11-04 15:20:07 +0800

    Updates

---
+++ precache-manifest.4783f6e88e7bdb6e59660f3aed7a0876.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16c31fc74a54ea87c0c2698cbb0a22d8",
+    "url": "/gltf-model/index.html"
+  },
+  {
+    "revision": "9666f7ff0319d250dbf5",
+    "url": "/gltf-model/static/css/main.19fd002b.chunk.css"
+  },
+  {
+    "revision": "beaaf91f5a6326243ab4",
+    "url": "/gltf-model/static/js/2.51165d86.chunk.js"
+  },
+  {
+    "revision": "9666f7ff0319d250dbf5",
+    "url": "/gltf-model/static/js/main.d2006473.chunk.js"
+  },
+  {
+    "revision": "c5fb65e08aa79dd86f30",
+    "url": "/gltf-model/static/js/runtime-main.adeaa7bb.js"
+  },
+  {
+    "revision": "2a2a458e0c321220d518a629d04e99af",
+    "url": "/gltf-model/static/media/mask.2a2a458e.gltf"
+  }
+]);
